c62e4c659895824686c934c746e93a4c879ccb73
aasmaea <aareaasmae@gmail.com>
2023-07-10 15:15:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.text('🥑🍞 Avocasdo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list=my_fruit_list(set_index('Fruit'))
+my_fruit_list=my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
