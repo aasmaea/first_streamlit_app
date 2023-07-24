150c2acd522b4433223299ad1e526776de076dd9
aasmaea <aareaasmae@gmail.com>
2023-07-24 13:57:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'kiwi')
 # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
